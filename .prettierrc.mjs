d4103a4170fcc123a2a189aa8237e2b4f3725ce8
Ryota Ikezawa <pavegy@gmail.com>
2023-08-06 05:48:08 +0900

    :recycle: Update initialize setting

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+const config = {
+  endOfLine: 'lf',
+  bracketSameLine: false,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
+
+export default config;
