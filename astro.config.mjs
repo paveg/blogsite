c10fd08234b43f02947334d1499837d03b1617c3
Ryota Ikezawa <pavegy@gmail.com>
2023-08-07 01:32:42 +0900

    :recycle: Fix contrast

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://www.funailog.com',
   integrations: [mdx(), sitemap(), tailwind()],
 });
