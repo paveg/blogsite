c67acfa5c1eb1c5fb2af1e9be71ad4345f5dfd3d
Ryota Ikezawa <pavegy@gmail.com>
2023-08-10 02:44:39 +0900

    :recycle: Add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
@@ -6,5 +7,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.funailog.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
