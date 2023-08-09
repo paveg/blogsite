f241194aae8db2a887ff1413bfea0f39abc0e80f
Ryota Ikezawa <pavegy@gmail.com>
2023-08-10 07:04:55 +0900

    :recycle: Update

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,6 +16,7 @@ const blog = defineCollection({
       .transform((str) => (str ? new Date(str) : undefined)),
     heroImage: z.string().optional(),
     isToc: z.boolean().optional() || false,
+    draft: z.boolean().optional() || true,
   }),
 });
 
